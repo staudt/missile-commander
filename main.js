f34d0c334e88c4d57f180ed40b225a29993a56fd
staudt <ricardost@gmail.com>
2015-09-05 11:33:36 -0300

    Basic features are working now

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 	var Scene = com.dgsprb.quick.Scene;
 
 	var gameScene;
-	var casinhas = [];
+	var casinhas = [];	// use tiles intead?
 
 	function createExplosion(x, y) {
 		var explosion = new GameObject();
@@ -26,6 +26,42 @@
 		gameScene.add(explosion);
 	}
 
+	function createMeteor() {
+		var meteor = new GameObject();
+		meteor.setSize(6);
+		meteor.setColor("Yellow");
+		meteor.setSolid();
+		meteor.addTag("meteor");
+		meteor.setX(Quick.random(Quick.getCanvasWidth())+1);
+		var target = casinhas[Quick.random(casinhas.length-1)].getCenter();
+		meteor.setSpeedToPoint(Quick.random(2)+1, target);
+		meteor.setExpiration(1000);
+		meteor.setDelegate({
+			"onCollision": function(gameObject) {
+				if (gameObject.hasTag("casinha")) {
+					gameObject.expire();
+					createExplosion(gameObject.getX(), gameObject.getY());
+				}
+				meteor.expire();
+			},
+			"update": function() {
+				if (Quick.random(6)==0) {
+					var trail = new GameObject();
+					trail.setSize(4);
+					trail.setColor("Gray");
+					trail.setPosition(meteor.getX(), meteor.getY());
+					trail.setExpiration(40);
+					meteor.setLayerIndex(0);
+					gameScene.add(trail);
+				}
+				if (meteor.getY() > Quick.getCanvasHeight())
+					meteor.expire();
+			}
+		});
+		gameScene.add(meteor);
+
+	}
+
 	function main() {
 		Quick.setName("Missile Commander");
 		gameScene = new Scene();
@@ -35,6 +71,17 @@
 		background.setHeight(Quick.getCanvasHeight());
 		background.setWidth(Quick.getCanvasWidth());
 		gameScene.add(background);
+
+		for (var i=0; i<7; i++) {
+			var casinha  = new GameObject();
+			casinha.setSize(20);
+			casinha.setColor(i==3 ? "Orange" : "Green");
+			casinha.setSolid();
+			casinha.addTag("casinha");
+			casinha.setPosition((Quick.getCanvasWidth()/8)*(i+1), Quick.getCanvasHeight()-20);
+			gameScene.add(casinha);
+			casinhas.push(casinha);
+		}
 		
 		var pointer = Quick.getPointer() 
 		var cursor = new GameObject();
@@ -45,22 +92,10 @@
 			"update" : function() {
 				var position = pointer.getPosition();
 				cursor.setPosition(position.getX(), position.getY());
-				if (pointer.getPush()) {
+				if (pointer.getPush())
 					createExplosion(position.getX(), position.getY());
-					var meteor = new GameObject();
-					meteor.setSize(6);
-					meteor.setColor("Yellow");
-					meteor.setSolid();
-					meteor.setX(Quick.random(Quick.getCanvasWidth())+1);
-					meteor.setSpeedToPoint(1, pointer.getPosition());
-					meteor.setDelegate({
-						"onCollision": function(gameObject) {
-							console.log(gameObject);
-							meteor.expire();
-						}
-					});
-					gameScene.add(meteor);
-				}
+				if (Quick.random(30)==0)
+					createMeteor()
 			}
 		});
 		gameScene.add(cursor);
